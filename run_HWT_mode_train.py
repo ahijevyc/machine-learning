7bd7292ec9f1d160724ad059a4e8d76844084cfb
ahijevyc <ahijevyc@ucar.edu>
2022-02-10 15:34:03 -0700

    more walltime, training iterations

--- run_HWT_mode_train.py
+++ run_HWT_mode_train.py
@@ -4,16 +4,17 @@
 #PBS -N "training"
 #PBS -S /bin/csh
 #PBS -j oe
-#PBS -l walltime=01:00:00
+#PBS -l walltime=02:00:00
 #PBS -l gpu_type=v100
 #PBS -l select=1:ncpus=1:ngpus=1:mem=50GB
-#PBS -o run.out
+#PBS -o basic.out
 #PBS -q casper
 #PBS -V 
 
 cd /glade/work/ahijevyc/NSC_objects
 
-source /glade/work/ahijevyc/NSC_objects/20201220_casper_daa/bin/activate.csh
-#source /glade/work/ahijevyc/20190723/bin/activate.csh
+ncar_pylib /glade/work/ahijevyc/20201220_daa_casper
 
-time python HWT_mode_train.py --ep 50 --neurons 20 30 40 50  
+time python HWT_mode_train.py --nfit 100 --suite basic
+time python HWT_mode_train.py --nfit 100 --suite basic2
+time python HWT_mode_train.py --nfit 100 --suite long
