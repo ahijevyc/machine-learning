f6f73f4b9529bbe94bd9ff4cbbe53737268e7d9c
ahijevyc <ahijevyc@ucar.edu>
2022-06-29 11:18:44 -0600

    Make x and y official coords in mask and predictor DataFrame
    train_test_split_time part of scaling file name
    
    don't use storm mode unless "with_storm_mode" in suite.
    
    Save argparse.namespace in yaml. Call it config.yaml instead of columns.yaml.
    
    test cases are at or later than train_test_split_time. (not strictly later than)

--- train_stormrpts_dnn.py
+++ train_stormrpts_dnn.py
@@ -150,6 +150,8 @@ def main():
     height, width =  65,93
     mask = mask.reshape((height,width))
     mask = xarray.DataArray(mask,dims=["y","x"])
+    logging.info(f"Make x and y indices actual coordinates of mask")
+    mask = mask.assign_coords(dict(x=mask.x,y=mask.y))
     if False:
         ax = plt.axes(projection = G211.g211)
         xs = G211.xs
@@ -165,7 +167,7 @@ def main():
         scalingfile = f"/glade/work/ahijevyc/NSC_objects/{model}/scaling_values_all_HRRRX.pk"
     elif model == "NSC3km-12sec":
         ifile = f'{model}{glmstr}.par'
-        scalingfile = f"scaling_values_{model}.pk"
+        scalingfile = f"scaling_values_{model}_{train_test_split_time:%Y%m%d_%H%M}.pk"
 
     if os.path.exists(ifile):
         logging.info(f'reading {ifile}')
@@ -179,33 +181,37 @@ def main():
                 search_str = f'/glade/work/sobash/NSC_objects/HRRR_new/grid_data/grid_data_HRRR_d01_202[01]*00-0000.par' # HRRR, not HRRRX. no 2022 (yet) 
                 ifiles.extend(glob.glob(search_str))
         elif model == "NSC3km-12sec":
-            search_str = f'/glade/work/sobash/NSC_objects/grid_data/grid_data_{model}_d01_201*00-0000.par'
+            search_str = f'/glade/work/sobash/NSC_objects/grid_data/grid_data_{model}_d01_20*00-0000.par'
             ifiles = glob.glob(search_str)
 
         # remove largest neighborhood size (fields containing N7 in the name)
         df = pd.read_parquet(ifiles[0], engine="pyarrow")
-        columns = df.columns
+        columns2read = df.columns
         N7_columns = [x for x in df.columns if "-N7" in x]
-        if "noN7" in suite:
-            logging.debug(f"ignoring {len(N7_columns)} N7 columns: {N7_columns}")
-            columns = set(df.columns) - set(N7_columns)
+        logging.info(f"ignoring {len(N7_columns)} N7 columns: {N7_columns}")
+        columns2read = set(df.columns) - set(N7_columns)
 
-        # all columns including severe reports
+        # Read list of columns columns2read (includes severe reports)
         logging.info(f"Reading {len(ifiles)} {model} files {search_str}")
-        df = pd.concat( pd.read_parquet(ifile, engine="pyarrow", columns=columns) for ifile in ifiles)
+        df = pd.concat( pd.read_parquet(ifile, engine="pyarrow", columns=columns2read) for ifile in ifiles)
         logging.info("done")
 
         # Read mode probabilities
-        search_str = f'/glade/scratch/cbecker/NCAR700_objects/output_object_based/evaluation/20*/label_probabilities_201*00_fh_*.nc'
+        search_str = f'/glade/scratch/cbecker/NCAR700_objects/output_object_based/evaluation/20*/label_probabilities_20*00_fh_*.nc'
         ifiles = sorted(glob.glob(search_str))
-        logging.info(f"Read and merge {len(ifiles)} storm mode files")
+        logging.info(f"Read {len(ifiles)} storm mode files")
         modeds = xarray.open_mfdataset( ifiles, preprocess=lambda x: x.set_index(time=['init_time','forecast_hour']) )
+        logging.info(f"Unstack time dimension")
         modeds = modeds.unstack('time')
+        logging.info(f"Make x and y indices actual coordinates, so we don't lose track when we mask and drop")
+        modeds = modeds.assign_coords(dict(x=modeds.x,y=modeds.y))
+        logging.info(f"Use CONUS mask and drop points outside CONUS")
         modeds = modeds.where(mask, drop=True)
+        logging.info(f"reset xarray Dataset coordinates [lon, lat] to variables")
         modeds = modeds.reset_coords(["lon","lat"]) 
 
         # In modeds, x : west to east, y : south to north
-        # In sobash df, x : south to north, y : west to east.
+        # In sobash df, xind : south to north, yind : west to east.
         modeds = modeds.rename(dict(x="y",y="x",init_time="initialization_time")) #dimensions are renamed to match df.
 
         # Index df and modeds the same way. 
@@ -239,6 +245,8 @@ def main():
         df = df.merge(glmds.to_dataframe(), on=df.index.names)
         assert not df.empty, f"Merged {model}/GLM Dataset is empty."
     
+
+    # Convert distance to closest storm report to True/False based on distance and time thresholds 
     df, rptcols = rptdist2bool(df, rptdist, twin)
 
     plotclimo=False
@@ -264,16 +272,34 @@ def main():
         rptcols.append("flashes")
 
 
-    labels = df[rptcols] # converted to Boolean above
-    df = df.drop(columns=rptcols)
 
-    if (df["HAILCAST_DIAM_MAX"] == 0).all():
+    if "HAILCAST_DIAM_MAX" in df and (df["HAILCAST_DIAM_MAX"] == 0).all():
         logging.info("HAILCAST_DIAM_MAX all zeros. Dropping.")
         df = df.drop(columns="HAILCAST_DIAM_MAX")
 
 
+
+    if "with_storm_mode" not in suite:
+        logging.info("making sure predictors don't include storm mode")
+        storm_mode_columns = [x for x in df.columns if "SS_" in x or "NN_" in x]
+        df = df.drop(columns=storm_mode_columns)
+
+
+
+
+
+
+    # Split labels away from predictors
+    labels = df[rptcols] # converted to Boolean above
+    df = df.drop(columns=rptcols)
+
+
+
+
+
+
     df.info()
-    labels.info()
+    print(labels.sum())
 
     # Split into training and testing cases
     # HRRRv3 to v4 at 20201202 0z.
@@ -281,7 +307,7 @@ def main():
         idate = df.initialization_time.astype('datetime64[ns]')
         df = df.drop(columns="initialization_time")
         logging.info(f"train test split time {train_test_split_time}")
-        test_idx  = (idate > train_test_split_time) & (idate < pd.to_datetime("20211031")) # Mar 1 - Oct 31 2021 
+        test_idx  = (idate >= train_test_split_time) & (idate < pd.to_datetime("20211031")) # Mar 1 - Oct 31 2021 # changed > to >= on Jun 28, 2022
         train_idx = ~test_idx
         df_train = df[train_idx]
         df_test  = df[test_idx]
@@ -334,14 +360,16 @@ def main():
                 epochs=epochs, validation_data=(df_test.to_numpy(dtype='float32'), test_labels.to_numpy(dtype='float32')), verbose=2)
             logging.debug(f"saving {model_i}")
             model.save(model_i)
-            # Save order of columns, scaling factors 
-            with open(os.path.join(model_i, "columns.yaml"), "w") as file:
+            # Save order of columns, scaling factors
+            # TODO: save split_train_test time too. (perhaps all arguments arg).
+            with open(os.path.join(model_i, "config.yaml"), "w") as yfile:
                 yaml.dump(
                         dict(columns=df_train.columns.to_list(),
                             mean=sv.loc["mean"].reindex(df_train.columns).to_list(),
                             std=sv.loc["std"].reindex(df_train.columns).to_list(),
-                            labels=train_labels.columns.to_list()
-                            ), file)
+                            labels=train_labels.columns.to_list(),
+                            args=args,
+                            ), yfile)
 
 
 if __name__ == "__main__":
