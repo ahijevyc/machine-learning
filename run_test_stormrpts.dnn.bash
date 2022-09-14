bb6b37f95824da528dc6b469e68479b8e0b196dc
ahijevyc <ahijevyc@ucar.edu>
2022-09-14 11:22:05 -0600

    less expensive options for NSC3km-12sec

--- run_test_stormrpts.dnn.bash
+++ run_test_stormrpts.dnn.bash
@@ -7,11 +7,20 @@ if [ $# -eq 1 ] ; then
 fi
 
 
-i=0
+i=0 # number each line of command file
+ncpus=8
+mem=120GB
+walltime=3:00:00
 
 # Loop through commands file one line at a time
 while read -r line
 do
+
+    if [[ $line == *"--model NSC3km-12sec"* ]]; then
+        mem=60GB
+        ncpus=5
+        walltime=0:50:00
+    fi
 
     cat <<EOS | qsub
 #!/bin/csh
@@ -19,9 +28,9 @@ do
 #PBS -N $i.test
 #PBS -S /bin/csh
 #PBS -j oe
-#PBS -l walltime=3:00:00
+#PBS -l walltime=$walltime
 #PBS -l gpu_type=v100
-#PBS -l select=1:ncpus=10:mem=130GB
+#PBS -l select=1:ncpus=$ncpus:mem=$mem
 #PBS -o $i.test.out
 #PBS -q casper
 
@@ -30,7 +39,7 @@ cd /glade/work/ahijevyc/NSC_objects
 module load conda # if you need conda environment.
 conda activate tf
 
-python test_stormrpts_dnn.py --nprocs 10 $line
+python test_stormrpts_dnn.py --nprocs $ncpus $line
 EOS
     let "i=i+1"
 done < $cmdfile
