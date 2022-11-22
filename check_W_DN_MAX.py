831634cfef939eb23330df656952d1f35eff7cbf
ahijevyc <ahijevyc@ucar.edu>
2022-11-22 14:38:48 -0700

    datadirs more complex
    logging

--- check_W_DN_MAX.py
+++ check_W_DN_MAX.py
@@ -1,22 +1,26 @@
 import glob
+import logging
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pdb
 import os
 
-datadirs = ["grid_data","grid_data_new"] 
-fig, axes = plt.subplots(ncols=len(datadirs), figsize=(len(datadirs)*4,4))
+datadirs = ["HRRR/grid_data","HRRR/grid_data_new","HRRR/grid_data"] 
+datadirs = ["HRRR_new/grid_data/grid_data_HRRR_", "HRRR_new/grid_data/grid_data_HRRRX_"]
+
+columns = ["W_UP_MAX", "W_DN_MAX","W_UP_MAX-N5T5","W_DN_MAX-N5T5"]
+
+fig, axes = plt.subplots(ncols=len(datadirs), figsize=(len(datadirs)*4,len(columns)))
 
 for ax, grid_data in zip(axes, datadirs): 
-    search_str = f'/glade/work/sobash/NSC_objects/HRRR/{grid_data}/*_HRRRX_d01_20*par'
+    search_str = f'/glade/work/sobash/NSC_objects/{grid_data}*.par'
     files = glob.glob(search_str)
     print(f"{len(files)} files {search_str}")
-    columns = ["W_UP_MAX", "W_DN_MAX","W_UP_MAX-N5T5","W_DN_MAX-N5T5"]
     df = pd.concat([pd.read_parquet(f, columns=columns) for f in files])#, index=files)
     print(df.describe())
     p = df.boxplot(ax=ax)
-    p.set_title(search_str, fontsize="small")
+    p.set_title(grid_data, fontsize="small")
 
 ofile = "W_DN_MAX_boxplot.png"
 plt.savefig(ofile)
