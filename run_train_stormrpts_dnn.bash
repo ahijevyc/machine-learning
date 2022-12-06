c82b228db19dae74fcb51907576e056290d62611
ahijevyc <ahijevyc@ucar.edu>
2022-12-06 10:50:47 -0700

    simply and accommodate large parquet input in driver scripts

--- run_train_stormrpts_dnn.bash
+++ run_train_stormrpts_dnn.bash
@@ -17,35 +17,34 @@ fi
 
 echo read command file $cmdfile
 
-# keep track of cmd line
-i=0
 mem=140GB
-walltime=2:45:00
+walltime=1:30:00
 
 # Loop through commands file one line at a time
+# enumerate the cmd lines starting with zero
+i=0
 while read -r line
 do
-    if [[ $line == *"--model NSC3km-12sec"* ]]; then
-        mem=63GB
+    if [[ $line == *"--model NSC"* ]]; then
+        mem=70GB
         walltime=0:20:00
     fi
-    # Use GPU for 1024-neuron model for speed.
-    # Run each fold separately or else you run out of memory on GPU
-    if [[ $line == *"--neurons 1024"* || $line == *"--neurons 256"* ]]; then
-        for fold in 0 1 2 3 4
-        do
-            if [[ $line == *"--kfold 1"* && $fold > 0 ]]; then
-                continue
-            fi
-            cat <<EOS | qsub
+    # GPU is faster for large n-neuron model.
+    # Tried each fold separately for less memory on GPU, but still not able to fit.
+    for fold in 0 1 2 3 4
+    do
+        # Skip subsequent folds if kfold == 1
+        if [[ $line == *"--kfold 1"* && $fold > 0 ]]; then
+            continue
+        fi
+        cat <<EOS | qsub
 #!/bin/csh
 #PBS -A NMMM0021
 #PBS -N $i.$fit.$fold
 #PBS -S /bin/csh
 #PBS -j oe
-#PBS -l walltime=0:45:00
-#PBS -l gpu_type=v100
-#PBS -l select=1:ncpus=1:ngpus=1:mem=$mem
+#PBS -l walltime=$walltime
+#PBS -l select=1:ncpus=1:mem=$mem
 #PBS -o $i.$fit.$fold.out
 #PBS -q casper
 
@@ -56,31 +55,9 @@ conda activate tf
 
 python train_stormrpts_dnn.py --fits $fit --folds $fold $line 
 EOS
-        done
+    done
 
-    else 
-        cat <<EOS | qsub
-#!/bin/csh
-#PBS -A NMMM0021
-#PBS -N $i.$fit
-#PBS -S /bin/csh
-#PBS -j oe
-#PBS -l walltime=$walltime
-#PBS -l gpu_type=v100
-#PBS -l select=1:ncpus=1:mem=$mem
-#PBS -o $i.$fit.out
-#PBS -q casper
-
-cd /glade/work/ahijevyc/NSC_objects
-
-module load conda # if you need conda environment.
-conda activate tf
-
-python train_stormrpts_dnn.py --fits $fit $line 
-EOS
-    fi
     let "i=i+1"
 done < $cmdfile
-
 
 echo finished command file $cmdfile
