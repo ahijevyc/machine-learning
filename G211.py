0bf128882f5efd544be968d02e03c848f5e5b972
ahijevyc <ahijevyc@ucar.edu>
2022-12-07 14:40:52 -0700

    half-spacing (40km) G211

--- G211.py
+++ G211.py
@@ -5,7 +5,7 @@ import numpy as np
 g211 = cartopy.crs.LambertConformal(central_longitude=-95, standard_parallels=(25,25))
 width=93
 height=65
-n = width*height
+
 llcrnrlon = -133.459
 llcrnrlat=12.19
 urcrnrlon=-49.38641
@@ -22,4 +22,20 @@ xv, yv = np.meshgrid(xs,ys)
 llz = cartopy.crs.PlateCarree().transform_points(g211,xv,yv)
 lon = llz[:,:,0]
 lat = llz[:,:,1]
+
+def x2():
+    """
+    Half spacing (40km) compared to G211 (80km)
+    """
+    global width, height, projected_corners
+    width=width*2
+    height=height*2
+
+    xs = np.linspace( projected_corners[0, 0], projected_corners[1, 0], width)
+    ys = np.linspace( projected_corners[0, 1], projected_corners[1, 1], height)
+    xv, yv = np.meshgrid(xs,ys)
+    llz = cartopy.crs.PlateCarree().transform_points(g211,xv,yv)
+    lon = llz[:,:,0]
+    lat = llz[:,:,1]
+    return lon, lat
 
