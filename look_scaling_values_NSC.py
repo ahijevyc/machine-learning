e0a8b22465b4609d8879e51d127a8b888fcff961
ahijevyc <ahijevyc@ucar.edu>
2023-01-05 12:26:04 -0700

    compare mean/std of NSC 1- 3- and 15-km

---
+++ look_scaling_values_NSC.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import seaborn as sns
+
+"""
+Compare scaling values of different training sets.
+Mean, std, etc.
+"""
+
+sns.set_theme()
+gs = ["1km", "3km-12sec", "15km"]
+ifiles = ["scaling_values_NSC" + x + "_20160701_1200.pk" for x in gs]
+df = pd.concat([pd.read_pickle(f) for f in ifiles], axis=0, keys=gs, names=["gs","stat"])
+
+nrows=3
+ncols=4
+n = nrows*ncols
+fig, axes = plt.subplots(nrows=nrows,ncols=ncols, figsize=(15,10), sharex=True)
+for i,c in enumerate(df.columns):
+    df[c].unstack().drop(columns="count").plot.bar(ax=axes.flatten()[i%n],legend=i%n == 0, title=c)
+    # final axes in figure
+    if i % n == n-1:
+        ofile = os.path.realpath(f"NSC1-3-15_scaling_values/NSC_scaling_values{i/n:02.0f}.png")
+        plt.savefig(ofile)
+        print(f"created {ofile}")
+        [a.clear() for a in axes.flatten()]
