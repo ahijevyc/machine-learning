47fa9bb6660807b97022fdba4a3a879432711ff0
ahijevyc <ahijevyc@ucar.edu>
2023-01-06 12:13:59 -0700

    use parse_args(cmd.split())

--- missing_models.py
+++ missing_models.py
@@ -1,43 +1,56 @@
+"""
+To explore sensitivity to ML hyperparamaters, hundreds of models are trained. 
+These hyperparameter combinations are listed in text files like cmds.txt.
+
+This script searches for models in the list that have not been created. 
+"""
+
 import argparse
+import glob
+import logging
 from ml_functions import get_argparser, savedmodel_default
 import os
 import pdb
+import sys
 from train_stormrpts_dnn import make_fhr_str
 
-cmds = open("cmds.txt","r").readlines()
+if len(sys.argv) == 2:
+    ifile = sys.argv[1]
+else:
+    ifile = "cmds.txt"
+cmds = open(ifile,"r").readlines()
 
 fhr = list(range(1,49))
-nfits = 5
-for i in range(nfits):
-    c = f"cmds_{i}.txt"
-    if os.path.exists(c):
-        os.remove(c)
 
-for icmd, cmd in enumerate(cmds):
+c = f"missing_model_cmds.txt"
+if os.path.exists(c):
+    logging.warning(f"removing {c}")
+    os.remove(c)
+
+for cmd in cmds:
     words = cmd.split()
     parser = get_argparser()
-    args = parser.parse_args()
-    args.glm = True
-    args.neurons = [int(words[2])]
-    args.layers = int(words[4])
-    args.optimizer = words[6]
-    args.learning_rate = float(words[8])
-    args.dropout = float(words[10])
-    args.reg_penalty = float(words[12])
-    args.epochs = int(words[14])
-    if words[-1] == "--batchnorm": args.batchnorm=True
-    fhr = list(range(1,49))
-    kfold = args.kfold
-    savedmodel = savedmodel_default(args, make_fhr_str(fhr) )
-    scores = f"nn/nn_{savedmodel}.{kfold}fold.scores.txt"
-    if not os.path.exists(scores): print(scores)
-    for i in range(nfits):
-        missing = False
-        for ifold in range(kfold):
-            model_i = f"nn/nn_{savedmodel}_{i}/{kfold}fold{ifold}"
-            if not os.path.exists(model_i) or not os.path.exists(f"{model_i}/config.yaml"): missing = True
-        if missing:
-            f = open(f"cmds_{i}.txt", "a")
-            f.write(cmd)
-            f.close()
-
+    args = parser.parse_args(cmd.split())
+    savedmodel = savedmodel_default(args, fhr_str=make_fhr_str(fhr))
+    teststart = args.teststart.strftime('%Y%m%d%H')
+    testend = args.testend.strftime('%Y%m%d%H')
+    scores = f"nn/nn_{savedmodel}.{args.kfold}fold.{teststart}-{testend}scores.txt"
+    search_str = scores.replace(f"{teststart}-{testend}","*")
+    score_files = glob.glob(search_str)
+    missing = False
+    if len(score_files) == 1:
+        pass
+    elif len(score_files) > 1:
+        logging.warning(f"found {len(score_files)} score files matching {search_str}")
+    else:
+        logging.error(f"No score file for {search_str}")
+        missing = True
+    for i in range(args.nfits):
+        for ifold in range(args.kfold):
+            model_i = f"nn/nn_{savedmodel}_{i}/{args.kfold}fold{ifold}"
+            if not os.path.exists(model_i) or not os.path.exists(f"{model_i}/config.yaml"):
+                missing = True
+    if missing:
+        f = open(c, "a")
+        f.write(cmd)
+        f.close()
