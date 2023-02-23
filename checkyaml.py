37943f7257f33bb07f8a05a611f84479e70185eb
ahijevyc <ahijevyc@ucar.edu>
2023-02-23 11:11:52 -0700

    print filename and model too

--- checkyaml.py
+++ checkyaml.py
@@ -10,22 +10,25 @@ import yaml
 
 for line in sys.stdin:
     ifile = line.rstrip()
+    print(ifile, end=" ")
     with open(ifile, "rb") as stream:
         yl = yaml.load(stream, Loader=yaml.Loader)
 
     # Namespace
     args = yl["args"]
 
-    print(ifile, end="")
+    model = args.model
+
     for arg in ["trainstart", "trainend", "teststart", "testend"]:
         if hasattr(args, arg):
-            print(f"  {arg}={getattr(args,arg).strftime('%Y%m%dT%H')}", end="")
+            print(f"  {arg}={getattr(args,arg).strftime('%Y%m%dT%H')}", end=" ")
     columns = yl["columns"]
     if "forecast_hour" in columns: 
         fhr_scaling_factor_mean = yl["mean"][columns.index("forecast_hour")]
-        if fhr_scaling_factor_mean != np.mean(args.fhr):
+        if model != "HRRR" and fhr_scaling_factor_mean != np.mean(args.fhr):
             logging.warning(f"{ifile} fhr mean scaling factor {fhr_scaling_factor_mean} does not equal mean of requested fhrs {args.fhr}")
         fhr_str = make_fhr_str(args.fhr)
-        print(f'  {fhr_str}', end="")
-    print(yl["labels"], end="")
+        #print(f' {fhr_str}', end=" ")
+    print(yl["labels"], end=" ")
+    print(args.twin, end=" ")
     print()
