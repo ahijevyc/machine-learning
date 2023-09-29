59bacb990f20592ae3a4509e5af6df5223ebbfe6
ahijevyc <ahijevyc@ucar.edu>
2023-09-29 11:36:05 -0600

    make object out of x2. subtract 1 to line up with 80-km grid

--- G211.py
+++ G211.py
@@ -1,11 +1,16 @@
-import cartopy
+import cartopy.crs as ccrs
 import numpy as np
+import os
 import pickle
 import xarray
 
-g211 = cartopy.crs.LambertConformal(central_longitude=-95, standard_parallels=(25,25))
-width=93
-height=65
+g211 = ccrs.LambertConformal(central_longitude=-95, standard_parallels=(25,25))
+
+# TODO: Depreciate width and height in favor of nlon and nlat
+nlon = 93
+nlat = 65
+width = nlon
+height = nlat
 
 ll_lon = -133.459
 ll_lat=12.19
@@ -14,38 +19,48 @@ ur_lat=57.2894
 
 # lower left and upper right projection coordinates
 (llx, lly, llz), (urx ,ury, urz) = g211.transform_points(
-    cartopy.crs.PlateCarree(),
+    ccrs.PlateCarree(),
     np.array([ll_lon, ur_lon]),
     np.array([ll_lat, ur_lat])
     )
 
 # gridded projection coordinates xv, yv
-xs = np.linspace( llx, urx, width)
-ys = np.linspace( lly, ury, height)
+xs = np.linspace( llx, urx, nlon)
+ys = np.linspace( lly, ury, nlat)
 xv, yv = np.meshgrid(xs,ys)
 # gridded lat/lon coordinates lat, lon
-ll3 = cartopy.crs.PlateCarree().transform_points(g211,xv,yv)
+ll3 = ccrs.PlateCarree().transform_points(g211,xv,yv)
 lon = ll3[:,:,0]
 lat = ll3[:,:,1]
 
 mask = pickle.load(open('/glade/u/home/ahijevyc/HRRR/usamask_mod.pk', 'rb'))
-mask = xarray.DataArray(mask.reshape((height,width)), 
-        coords=dict(y=range(height), x=range(width)), dims=["y","x"])
+mask = xarray.DataArray(mask.reshape((nlat,nlon)), 
+        coords=dict(y=range(nlat), x=range(nlon)), dims=["y","x"])
 
 # TODO: clean up this kludge
-def x2():
+class x2():
     """
     Half spacing compared to G211
     """
     global ll_lon, ll_lat, ur_lon, ur_lat
-    width=93 * 2
-    height=65 * 2
+    def __init__(self):
+        nlon = 93 * 2 - 1 
+        nlat = 65 * 2 - 1 # subtract 1 to line up with 80 km grid (started Sep 19 2023)
 
-    xs = np.linspace( llx, urx, width)
-    ys = np.linspace( lly, ury, height)
-    xv, yv = np.meshgrid(xs,ys)
-    ll3 = cartopy.crs.PlateCarree().transform_points(g211,xv,yv)
-    lon = ll3[:,:,0]
-    lat = ll3[:,:,1]
-    return lon, lat
+        xs = np.linspace( llx, urx, nlon)
+        ys = np.linspace( lly, ury, nlat)
+        self.xs = xs
+        self.ys = ys
+        xv, yv = np.meshgrid(xs,ys)
+        self.xv = xv
+        self.yv = yv
+        ll3 = ccrs.PlateCarree().transform_points(g211,xv,yv)
+        lon = ll3[:,:,0]
+        lat = ll3[:,:,1]
 
+        self.nlon = nlon
+        self.nlat = nlat
+        self.width = nlon
+        self.height = nlat
+        self.lon = lon
+        self.lat = lat
